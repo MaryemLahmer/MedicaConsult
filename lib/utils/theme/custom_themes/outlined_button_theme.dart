550624e487f9cf8d7e0efccb41ffeb1f23b1642a
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-03-25 05:17:51 +0100

    defining components themes

---
+++ lib/utils/theme/custom_themes/outlined_button_theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+///--- Outlined Light & Dark Button Themes ---
+class MedicaOutlinedButtonTheme{
+  MedicaOutlinedButtonTheme._();
+
+
+  /// Light Theme
+  static final lightOutlinedButtonTheme=OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.black,
+      side: const BorderSide(color: Colors.blue),
+      textStyle: const TextStyle(fontWeight: FontWeight.w600,fontSize: 16,color: Colors.black),
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+    )
+  );
+
+  /// Dark Theme
+  static final darkOutlinedButtonTheme=OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(
+        elevation: 0,
+        foregroundColor: Colors.white,
+        side: const BorderSide(color: Colors.blueAccent),
+        textStyle: const TextStyle(fontWeight: FontWeight.w600,fontSize: 16,color: Colors.white),
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+      )
+  );
+}
