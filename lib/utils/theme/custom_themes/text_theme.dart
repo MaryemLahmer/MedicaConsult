4780741167c9155a9294c6d8560e4ce7c9627404
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-03-26 23:49:05 +0100

    fixing themes for text

--- lib/utils/theme/custom_themes/text_theme.dart
+++ lib/utils/theme/custom_themes/text_theme.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
+import '../../constants/colors.dart';
+
 class MedicaTextTheme{
   MedicaTextTheme._();
 
   /// Customizable light theme text
   static TextTheme lightTextTheme = TextTheme(
-    headlineLarge: const TextStyle().copyWith(fontSize: 32.0,fontWeight: FontWeight.bold,color: Colors.black),
+    headlineLarge: const TextStyle().copyWith(fontSize: 32.0,fontWeight: FontWeight.bold,color: MedicaColors.primary),
     headlineMedium: const TextStyle().copyWith(fontSize: 24.0,fontWeight: FontWeight.w600,color: Colors.black),
     headlineSmall: const TextStyle().copyWith(fontSize: 18.0,fontWeight: FontWeight.w600,color: Colors.black),
 
@@ -14,7 +16,7 @@ class MedicaTextTheme{
     titleSmall: const TextStyle().copyWith(fontSize: 16.0,fontWeight: FontWeight.w400,color: Colors.black),
 
     bodyLarge: const TextStyle().copyWith(fontSize: 14.0,fontWeight: FontWeight.w500,color: Colors.black),
-    bodyMedium: const TextStyle().copyWith(fontSize: 12.0,fontWeight: FontWeight.w400,color: Colors.black),
+    bodyMedium: const TextStyle().copyWith(fontSize: 12.0,fontWeight: FontWeight.w400,color: Colors.black,),
     bodySmall: const TextStyle().copyWith(fontSize: 14.0,fontWeight: FontWeight.w500,color: Colors.black.withOpacity(0.5)),
 
     labelLarge:const TextStyle().copyWith(fontSize: 12.0,fontWeight: FontWeight.normal,color: Colors.black),
@@ -24,7 +26,7 @@ class MedicaTextTheme{
 
   /// Customizable dark theme text
   static TextTheme darkTextTheme = TextTheme(
-    headlineLarge: const TextStyle().copyWith(fontSize: 34.0,fontWeight: FontWeight.bold,color: Colors.white),
+    headlineLarge: const TextStyle().copyWith(fontSize: 34.0,fontWeight: FontWeight.bold,color: MedicaColors.primary),
     headlineMedium: const TextStyle().copyWith(fontSize: 22.0,fontWeight: FontWeight.w400, color: Colors.white),
     headlineSmall: const TextStyle().copyWith(fontSize: 18.0,fontWeight: FontWeight.w600,color: Colors.white),
 
