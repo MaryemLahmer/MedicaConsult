6375b8650a33a7a608515097333ec87eb1d7c8d9
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-03-28 16:53:34 +0100

    creating settings menu tile

---
+++ lib/features/personalization/screens/settings/widgets/settings_menu_tile.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:medica_consult/utils/constants/colors.dart';
+import 'package:medica_consult/utils/constants/sizes.dart';
+import 'package:medica_consult/utils/helpers/helper_functions.dart';
+
+class SettingsMenuTile extends StatelessWidget {
+  const SettingsMenuTile({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.onPressed,
+    required this.trailingIcon,
+  });
+
+  final IconData icon;
+  final IconData trailingIcon;
+  final String title;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = MedicaHelperFunctions.isDarkMode(context);
+    return ListTile(
+        leading: Icon(
+          icon,
+          size: 28,
+          color: dark? MedicaColors.white:MedicaColors.primary ,
+        ),
+        title: Text(
+          title,
+          style: Theme.of(context).textTheme.headlineSmall
+        ),
+        trailing: IconButton(
+          icon: Icon(
+            trailingIcon,
+            color: dark? MedicaColors.white:MedicaColors.primary ,
+            size: 28,
+          ),
+          onPressed: onPressed,
+        ));
+  }
+}
