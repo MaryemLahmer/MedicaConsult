7a982dd27e5a1255b56fa6d5b2e343878c1c7f54
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-03-29 03:35:08 +0100

    customizing appbar

--- lib/common/widgets/appbar/custom_appbar.dart
+++ lib/common/widgets/appbar/custom_appbar.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:medica_consult/features/authentication/screens/onboarding_screens/option_screen.dart';
 import 'package:medica_consult/utils/constants/sizes.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
-
+import 'package:medica_consult/utils/helpers/helper_function.dart';
+import '../../../utils/constants/colors.dart';
 import '../../../utils/device/device_utility.dart';
 
 class MedicaAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -22,17 +24,22 @@ class MedicaAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dark = MedicaHelperFunctions.isDarkMode(context);
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: MedicaSizes.md),
       child: AppBar(
         automaticallyImplyLeading: false,
         leading: showBackArrow
             ? IconButton(
+                icon: Icon(
+                  Iconsax.arrow_left,
+                  color: dark ? MedicaColors.white : MedicaColors.black,
+                ),
                 onPressed: () => Get.back(),
-                icon: const Icon(Iconsax.arrow_left))
+              )
             : leadingIcon != null
                 ? IconButton(
-                    onPressed: leadingOnPressed, icon: Icon(leadingIcon))
+                    onPressed: () => Get.back(), icon: Icon(leadingIcon))
                 : null,
         title: title,
         actions: actions,
