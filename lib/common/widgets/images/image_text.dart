fcb41ecb1b153f077f99918579b1df4f1609b764
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-03-29 04:24:35 +0100

    creating clickable image with text

---
+++ lib/common/widgets/images/image_text.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import '../../../utils/constants/colors.dart';
+import '../../../utils/constants/sizes.dart';
+import '../../../utils/helpers/helper_functions.dart';
+class VerticalImageText extends StatelessWidget {
+  const VerticalImageText({
+    super.key,
+    required this.image,
+    required this.title,
+    this.backgroundColor,
+    this.onTap,
+  });
+
+  final String image, title;
+  final Color? backgroundColor;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.only(right: MedicaSizes.spaceBetweenItems),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            /// Circular Icon
+            Container(
+              height: 55,
+              width: 70,
+              padding: const EdgeInsets.all(MedicaSizes.sm),
+              decoration: BoxDecoration(
+                color: backgroundColor ??
+                    (MedicaHelperFunctions.isDarkMode(context)
+                        ? MedicaColors.black
+                        : MedicaColors.white),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Center(
+                child: Image(
+                  image: AssetImage(image),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+
+            /// Text
+            const SizedBox(
+              height: MedicaSizes.spaceBetweenItems / 2,
+            ),
+            SizedBox(
+              width: 80,
+              child: Center(
+                child: Text(
+                  title,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyLarge!
+                      .apply(color: MedicaHelperFunctions.isDarkMode(context)
+                      ? MedicaColors.white
+                      : MedicaColors.black),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
