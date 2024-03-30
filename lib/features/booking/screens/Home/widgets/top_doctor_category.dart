93d46aa9a3124722043255c893c1225994a9bd32
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-03-30 01:09:30 +0100

    top doctor category widget

---
+++ lib/features/booking/screens/Home/widgets/top_doctor_category.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:medica_consult/features/personalization/screens/widgets/doctor_card.dart';
+import '../../../../../data/data.dart';
+import '../../../../../utils/logging/logger.dart';
+
+class TopDoctorCategory extends StatelessWidget {
+  const TopDoctorCategory({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 180,
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: 6,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (_, index) {
+          return Row(
+            children: doctorData.take(6).map((data) {
+              return DoctorCard(
+                profileImage: data['profileImage'],
+                name: data['name'],
+                speciality: data['speciality'],
+                rating: data['rating'],
+                distance: data['distance'],
+                onPressed: () {
+                  MedicaLoggerHelper.info(
+                      "Index: ${doctorData.indexOf(data)}");
+                },
+              );
+            }).toList(),
+          );
+        },
+      ),
+    );
+  }
+}
