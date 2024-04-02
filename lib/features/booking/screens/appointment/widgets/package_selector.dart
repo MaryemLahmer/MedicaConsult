f600f05692f15d917e072da77827ba5e7d818a2c
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-04-02 16:27:44 +0100

    create a customized switch tile list

---
+++ lib/features/booking/screens/appointment/widgets/package_selector.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:medica_consult/utils/constants/colors.dart';
+
+class MedicaSwitchListTile extends StatefulWidget {
+  const MedicaSwitchListTile({super.key, required this.icon, required this.consultation, required this.price});
+  final IconData icon;
+  final String consultation;
+  final String price;
+
+  @override
+  State<MedicaSwitchListTile> createState() => _MedicaSwitchListTileState();
+}
+
+class _MedicaSwitchListTileState extends State<MedicaSwitchListTile> {
+  bool isSwitched = false;
+
+  @override
+  Widget build(BuildContext context) {
+    IconData icon = widget.icon ;
+    String price = widget.price;
+    String consultation = widget.consultation;
+
+    return SwitchListTile(title: Expanded(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Expanded(child: Text(consultation,style: Theme.of(context).textTheme.bodyLarge!.apply(color: MedicaColors.black),)),
+          Text(price,style: Theme.of(context).textTheme.bodyMedium,),
+
+        ],
+      ),
+    ),
+        activeColor: MedicaColors.primary,
+        activeTrackColor: MedicaColors.accent,
+        secondary: Icon(icon,color: MedicaColors.primary,),
+        value: isSwitched, onChanged: (value){
+      setState(() {
+        isSwitched = value;
+      });
+    });
+  }
+}
