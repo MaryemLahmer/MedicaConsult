21a8ed19bf77c09f9ad1dd087b4459bee8880c2c
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-04-04 18:37:59 +0100

    adding  title invisibility feature

--- lib/common/widgets/appbar/app_logo_title.dart
+++ lib/common/widgets/appbar/app_logo_title.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:medica_consult/utils/constants/sizes.dart';
-
+import 'package:medica_consult/utils/constants/colors.dart';
 import '../../../utils/constants/image_strings.dart';
 import '../../../utils/constants/text_strings.dart';
 
 class AppLogoAndTitle extends StatelessWidget {
   const AppLogoAndTitle({
     super.key,
+    this.showTitle = true,
   });
+
+  final bool showTitle;
 
   @override
   Widget build(BuildContext context) {
@@ -19,10 +21,11 @@ class AppLogoAndTitle extends StatelessWidget {
           height: 150,
           image: AssetImage(MedicaImages.appLogo),
         ),
-        Text(
-          MedicaTexts.appName,
-          style: Theme.of(context).textTheme.headlineLarge,
-        ),
+        if (showTitle)
+          Text(
+            MedicaTexts.appName,
+            style: Theme.of(context).textTheme.headlineLarge!.apply(color: MedicaColors.primary),
+          ),
       ],
     );
   }
