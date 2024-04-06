2df75452b12ef8e27c6423e369b9c61a218044aa
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-04-06 16:26:27 +0100

    reports and prescriptions screen

---
+++ lib/features/personalization/screens/settings/reports_and_prescriptions.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:medica_consult/common/widgets/appbar/custom_appbar.dart';
+import 'package:medica_consult/features/personalization/screens/settings/prescription_tab.dart';
+import 'package:medica_consult/utils/constants/colors.dart';
+import 'package:medica_consult/utils/constants/sizes.dart';
+import 'package:medica_consult/utils/helpers/helper_function.dart';
+
+class ReportsAndPrescriptions extends StatelessWidget {
+  const ReportsAndPrescriptions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: MedicaAppBar(
+          title: Row(
+            children: [
+              Expanded(
+                child: Text(
+                  'Documents',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: Column(
+          children: [
+            TabBar(
+                labelColor: MedicaHelperFunctions.isDarkMode(context)
+                    ? MedicaColors.white
+                    : MedicaColors.primary,
+                indicatorColor: MedicaColors.primary,
+                unselectedLabelColor: MedicaColors.darkGrey,
+                isScrollable: false,
+                tabs: [
+                  Tab(
+                    child: Text(
+                      'Prescriptions',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  ),
+                  Tab(
+                    child: Text(
+                      'Reports',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  )
+                ]),
+            const Expanded(
+              child: TabBarView(children: [
+                MedicaPrescription(prescriptionList: ['p1','p2','p3'],report: false),
+                MedicaPrescription(prescriptionList: ['p1','p2',], report: true,),
+
+              ]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
