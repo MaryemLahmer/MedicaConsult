674938782ef28f43c9b6ced304d9a6c68f03f281
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-04-07 03:41:41 +0100

    adding theme provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:medica_consult/utils/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 import 'app.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const App(),
+
+  ));
 }
