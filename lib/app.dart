0fac9be8ba462bddc7d09bf482f8d13f4c1d5ed5
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-04-07 03:42:08 +0100

    adding theme provider for dark theme control

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'features/booking/screens/payment/widgets/payment_method_picker.dart';
+import 'package:medica_consult/utils/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
+import 'features/personalization/screens/settings/settings.dart';
 import 'utils/theme/theme.dart';
 
 //--Use this Class to setup themes, initial Bindings, any animations and much more
@@ -12,9 +14,10 @@ class App extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
-      theme: MedicaAppTheme.lightTheme,
-      darkTheme: MedicaAppTheme.darkTheme,
-      home: const PaymentMethodPicker(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
+      //theme: MedicaAppTheme.lightTheme,
+      //darkTheme: MedicaAppTheme.darkTheme,
+      home: const SettingsScreen(),
 
       ///show loader or circular progress indicator meanwhile authentication repository is deciding to show relevant screen
       ///home:const Scaffold(backgroundColor: MedicaColors.primary,body: Center(child: CircularProgressIndicator(color: Colors.white,),),)
