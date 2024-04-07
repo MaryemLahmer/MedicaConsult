42318958bb1967b4f55def3e3076782471b3a3ed
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-04-07 01:40:37 +0100

    create toggled setting widget

---
+++ lib/features/personalization/screens/settings/widgets/toggled_setting.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:medica_consult/utils/constants/colors.dart';
+import 'package:medica_consult/utils/constants/sizes.dart';
+
+class ToggledSetting extends StatefulWidget {
+  const ToggledSetting(
+      {super.key, required this.setting,});
+
+  final String setting;
+
+  @override
+  State<ToggledSetting> createState() => _ToggledSettingState();
+}
+
+class _ToggledSettingState extends State<ToggledSetting> {
+  bool activated = true;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+            flex: 3,
+            child: Text(widget.setting,
+                style: Theme.of(context).textTheme.headlineSmall)),
+        Switch(
+            activeColor: MedicaColors.primary,
+            value: activated,
+            onChanged: (value) {
+              setState(() {
+                activated = value;
+              });
+            }),
+      ],
+    );
+  }
+}
