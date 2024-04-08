d9ac31cb98c3eeee764ab4fadd4f59a3cfdbc6ed
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-04-08 23:24:15 +0100

    adjusting widget

--- lib/features/personalization/screens/settings/widgets/settings_menu_tile.dart
+++ lib/features/personalization/screens/settings/widgets/settings_menu_tile.dart
@@ -2,21 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:medica_consult/utils/constants/colors.dart';
 import 'package:medica_consult/utils/helpers/helper_functions.dart';
 import 'package:provider/provider.dart';
-
 import '../../../../../utils/theme/theme_provider.dart';
 
 class SettingsMenuTile extends StatefulWidget {
   const SettingsMenuTile({
     super.key,
-    required this.icon,
+    this.icon,
     required this.title,
     required this.onPressed,
-    required this.trailingIcon,
+    this.trailingIcon,
     this.toggle = false,
   });
 
-  final IconData icon;
-  final IconData trailingIcon;
+  final IconData? icon;
+  final IconData? trailingIcon;
   final String title;
   final VoidCallback onPressed;
   final bool toggle;
@@ -27,6 +26,7 @@ class SettingsMenuTile extends StatefulWidget {
 
 class _SettingsMenuTileState extends State<SettingsMenuTile> {
   bool isToggled = false;
+
   @override
   Widget build(BuildContext context) {
     final dark = MedicaHelperFunctions.isDarkMode(context);
@@ -36,23 +36,25 @@ class _SettingsMenuTileState extends State<SettingsMenuTile> {
           size: 28,
           color: dark ? MedicaColors.white : MedicaColors.primary,
         ),
-        title: Text(widget.title, style: Theme.of(context).textTheme.headlineSmall),
+        title: Text(widget.title,
+            style: Theme.of(context).textTheme.headlineSmall),
         trailing: IconButton(
-          icon: (widget.toggle != true)?
-          Icon(
-            widget.trailingIcon,
-            color: dark ? MedicaColors.white : MedicaColors.primary,
-            size: 28,
-          ):
-          Switch(value: isToggled,
-              activeColor: MedicaColors.primary,
-              onChanged: (value){
-            setState(() {
-              isToggled = value;
-              Provider.of<ThemeProvider>(context,listen:false).toggleTheme();
-
-            });
-              }),
+          icon: (widget.toggle != true)
+              ? Icon(
+                  widget.trailingIcon,
+                  color: dark ? MedicaColors.white : MedicaColors.primary,
+                  size: 28,
+                )
+              : Switch(
+                  value: isToggled,
+                  activeColor: MedicaColors.primary,
+                  onChanged: (value) {
+                    setState(() {
+                      isToggled = value;
+                      Provider.of<ThemeProvider>(context, listen: false)
+                          .toggleTheme();
+                    });
+                  }),
           onPressed: widget.onPressed,
         ));
   }
