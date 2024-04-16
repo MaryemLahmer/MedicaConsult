bd2e5476a92b8e0396cf7117acb2b9dcb9d08ea4
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-04-16 17:17:59 +0100

    paths

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:medica_consult/navigation.dart';
 import 'package:medica_consult/utils/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
-import 'features/personalization/screens/settings/settings.dart';
-import 'utils/theme/theme.dart';
+
 
 //--Use this Class to setup themes, initial Bindings, any animations and much more
 class App extends StatelessWidget {
@@ -15,9 +15,7 @@ class App extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
       theme: Provider.of<ThemeProvider>(context).themeData,
-      //theme: MedicaAppTheme.lightTheme,
-      //darkTheme: MedicaAppTheme.darkTheme,
-      home: const SettingsScreen(),
+      home: const NavigationMenu(),
 
       ///show loader or circular progress indicator meanwhile authentication repository is deciding to show relevant screen
       ///home:const Scaffold(backgroundColor: MedicaColors.primary,body: Center(child: CircularProgressIndicator(color: Colors.white,),),)
