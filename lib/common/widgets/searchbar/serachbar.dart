467f46b105e8dfad05a3a632418bf4adc94a84e0
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-04-16 17:14:48 +0100

    adjust search bar

--- lib/common/widgets/searchbar/serachbar.dart
+++ lib/common/widgets/searchbar/serachbar.dart
@@ -3,34 +3,40 @@ import '../../../utils/constants/colors.dart';
 
 class MedicaSearchBar extends StatelessWidget {
   const MedicaSearchBar({
-    super.key, required this.hintText,
+    super.key,
+    required this.hintText,
+    this.controller,
+    this.onChanged,
   });
 
   final String hintText;
+  final TextEditingController? controller;
+  final void Function(String)? onChanged;
+
   @override
   Widget build(BuildContext context) {
     return TextField(
+      onChanged: onChanged,
+      controller: controller,
       style: Theme.of(context).textTheme.bodyMedium,
       decoration: InputDecoration(
         filled: true,
         fillColor: MedicaColors.lightGrey,
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(36.0),
-          borderSide:
-          const BorderSide(color: MedicaColors.accent),
+          borderSide: const BorderSide(color: MedicaColors.accent),
         ),
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(36.0),
-          borderSide: const BorderSide(
-              color: MedicaColors.borderPrimary),
+          borderSide: const BorderSide(color: MedicaColors.borderPrimary),
         ),
         hintText: hintText,
         // Added search icon in the hint text
         hintStyle: const TextStyle(
           color: MedicaColors.textSecondary,
         ),
-        prefixIcon: const Icon(Icons
-            .search), // Adding the search icon using Icon widget
+        prefixIcon: const Icon(
+            Icons.search), // Adding the search icon using Icon widget
       ),
       cursorColor: MedicaColors.black,
     );
