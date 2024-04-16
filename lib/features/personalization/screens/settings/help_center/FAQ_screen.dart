b2cbdc1701e6ccbf324a4dfbf4262ee164709bba
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-04-16 17:17:18 +0100

    create FAQ screen

--- lib/features/personalization/screens/settings/help_center/FAQ_screen.dart
+++ lib/features/personalization/screens/settings/help_center/FAQ_screen.dart
@@ -1,26 +1,108 @@
 import 'package:flutter/material.dart';
-import 'package:medica_consult/common/widgets/searchbar/serachbar.dart';
+import 'package:medica_consult/features/personalization/screens/test/questions.dart';
+import 'package:medica_consult/utils/constants/colors.dart';
 import 'package:medica_consult/utils/constants/sizes.dart';
+import '../../../../../common/widgets/buttons/color_changing_button.dart';
+import '../widgets/expansion_tile.dart';
 
-import '../../../../../common/widgets/buttons/toggleable_button_list.dart';
+class FAQScreen extends StatefulWidget {
+  const FAQScreen({super.key});
 
+  @override
+  State<FAQScreen> createState() => _FAQScreenState();
+}
 
-class FAQScreen extends StatelessWidget {
-  const FAQScreen({
-    super.key,
-  });
+class _FAQScreenState extends State<FAQScreen> {
+  final controller = TextEditingController();
+  static List<String> options = ['General', 'Account', 'Service', 'Payment'];
+  List<String> pressedOptions = [];
+  static List<Question> listOfQuestions = allQuestions;
+  List<Question> displayList = List.from(listOfQuestions);
+
+  void updateList(String value) {
+    setState(() {
+        displayList = listOfQuestions
+            .where(
+                (q) => q.question.toLowerCase().contains(value.toLowerCase()))
+            .toList();
+       if (pressedOptions != []){
+         displayList = (listOfQuestions.where((q) => pressedOptions.contains(q.category)).toList());
+       }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
-      child: Padding(
-        padding: EdgeInsets.all(MedicaSizes.md),
-        child: Column(
-          children: [
-            ToggleableButtonList(),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(MedicaSizes.defaultSpace),
+          child: Column(
+            children: [
+              /// list of filters
+              SizedBox(
+                height: 80,
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: options.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (_, i) {
+                      return Row(
+                        children: [
+                          ColorChangingElevatedButton(
+                            activeColor: MedicaColors.primary,
+                            inactiveColor: MedicaColors.grey,
+                            text: options[i],
+                            selectedList: pressedOptions,
+                          )
+                        ],
+                      );
+                    }),
+              ),
 
-            MedicaSearchBar(hintText: "Search"),
-          ],
+              /// search Bar
+              TextField(
+                controller: controller,
+                onChanged: (value) => updateList(value),
+                style: Theme.of(context).textTheme.bodyMedium,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: MedicaColors.lightGrey,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(36.0),
+                    borderSide: const BorderSide(color: MedicaColors.accent),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(36.0),
+                    borderSide:
+                        const BorderSide(color: MedicaColors.borderPrimary),
+                  ),
+                  prefixIcon: const Icon(Icons.search),
+                  hintStyle: const TextStyle(
+                    color: MedicaColors.textSecondary,
+                  ), // Adding the search icon using Icon widget
+                ),
+                cursorColor: MedicaColors.black,
+              ),
+              const SizedBox(
+                height: MedicaSizes.spaceBetweenItems,
+              ),
+              SizedBox(
+                height: 500,
+                width: double.infinity,
+                child: Scaffold(
+                  body: ListView.builder(
+                      itemCount: displayList.length,
+                      itemBuilder: (context, index) {
+                        final questionWithAnswer = displayList[index];
+                        return MedicaExpansionTile(
+                            question: questionWithAnswer.question,
+                            answer: questionWithAnswer.answer);
+                      }),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
