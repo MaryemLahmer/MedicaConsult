33a4f892aabdbaabf6c97844ea3900d4a9ee3016
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-04-22 00:07:01 +0100

    add bindings

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:medica_consult/utils/constants/colors.dart';
 import 'package:medica_consult/utils/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
+import 'bindings/general_bindings.dart';
 
 
 //--Use this Class to setup themes, initial Bindings, any animations and much more
@@ -15,6 +16,7 @@ class App extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
       theme: Provider.of<ThemeProvider>(context).themeData,
+      //initialBinding: GeneralBindings(),
 
       ///show loader or circular progress indicator meanwhile authentication repository is deciding to show relevant screen
       home:const Scaffold(backgroundColor: MedicaColors.primary,body: Center(child: CircularProgressIndicator(color: Colors.white,),),)
