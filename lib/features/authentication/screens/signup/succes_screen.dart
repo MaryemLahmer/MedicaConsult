7c8fbdf464aed04ebcf532211c45ef6e6e54bb35
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-04-23 00:16:31 +0100

    adjust success screen

--- lib/features/authentication/screens/signup/succes_screen.dart
+++ lib/features/authentication/screens/signup/succes_screen.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 import 'package:medica_consult/common/styles/spacing_styles.dart';
-import 'package:medica_consult/utils/constants/image_strings.dart';
 import 'package:medica_consult/utils/constants/sizes.dart';
 import 'package:medica_consult/utils/constants/text_strings.dart';
-import 'package:medica_consult/utils/helpers/helper_function.dart';
-import 'package:medica_consult/features/authentication/screens/login/login.dart';
 
 class SuccessScreen extends StatelessWidget {
-  const SuccessScreen({super.key});
+  const SuccessScreen(
+      {super.key,
+      required this.image,
+      required this.title,
+      required this.subtitle,
+      required this.onPressed});
+
+  final String image, title, subtitle;
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -18,24 +23,19 @@ class SuccessScreen extends StatelessWidget {
           padding: MedicaSpacingStyle.paddingWithAppBarHeight * 2,
           child: Column(
             children: [
-              ///Image
-              Image(
-                image:
-                    const AssetImage(MedicaImages.deliveredEmailIllustration),
-                width: MedicaHelperFunctions.screenWidth() * 0.6,
-              ),
-
+              /// Image
+              Lottie.asset(image,
+                  width: MediaQuery.of(context).size.width * 0.6),
               const SizedBox(
                 height: MedicaSizes.spaceBetweenSections,
               ),
 
-              ///Title&Subtitle
-
-              Text(MedicaTexts.yourAccountCreatedTitle,
+              /// Title&Subtitle
+              Text(title,
                   style: Theme.of(context).textTheme.headlineMedium,
                   textAlign: TextAlign.center),
               const SizedBox(height: MedicaSizes.spaceBetweenItems),
-              Text(MedicaTexts.yourEmailCreatedSubtitle,
+              Text(subtitle,
                   style: Theme.of(context).textTheme.labelLarge,
                   textAlign: TextAlign.center),
               const SizedBox(height: MedicaSizes.spaceBetweenSections),
@@ -44,7 +44,7 @@ class SuccessScreen extends StatelessWidget {
               SizedBox(
                   width: double.infinity,
                   child: ElevatedButton(
-                      onPressed: () => Get.to(() => const LoginScreen()),
+                      onPressed: onPressed,
                       child: const Text(MedicaTexts.tContinue))),
             ],
           )),
