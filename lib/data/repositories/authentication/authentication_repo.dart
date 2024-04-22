9fba61d652b2b692cab2058f72fcd2267bab0dba
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-04-23 00:14:56 +0100

    update exceptions for authentication

--- lib/data/repositories/authentication/authentication_repo.dart
+++ lib/data/repositories/authentication/authentication_repo.dart
@@ -6,6 +6,11 @@ import 'package:get_storage/get_storage.dart';
 import 'package:medica_consult/features/authentication/screens/login/login.dart';
 import 'package:medica_consult/features/authentication/screens/onboarding_screens/onboarding.dart';
 
+import '../../../utils/exceptions/firebase_auth_exceptions.dart';
+import '../../../utils/exceptions/firebase_exceptions.dart';
+import '../../../utils/exceptions/format_exceptions.dart';
+import '../../../utils/exceptions/platform_exceptions.dart';
+
 class AuthenticationRepository extends GetxController {
   static AuthenticationRepository get instance => Get.find();
 
@@ -39,19 +44,35 @@ class AuthenticationRepository extends GetxController {
       return await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
     } on FirebaseAuthException catch (e) {
-      throw 'Something went wrong. Please try again!';
+      throw MedicaFirebaseAuthException(e.code).message;
     } on FirebaseException catch (e) {
-      throw 'Something went wrong. Please try again!';
+      throw MedicaFirebaseException(e.code).message;
     } on FormatException catch (_) {
-      throw 'Something went wrong. Please try again!';
+      throw const MedicaFormatException();
     } on PlatformException catch (e) {
-      throw 'Something went wrong. Please try again!';
+      throw MedicaPlatformException(e.code).message;
     } catch (e) {
       throw 'Something went wrong. Please try again!';
     }
   }
 
   /// [EmailAuthentication] - Mail Verification
+  Future<void> sendEmailVerification() async {
+    try {
+      await _auth.currentUser?.sendEmailVerification();
+    } on FirebaseAuthException catch (e) {
+      throw MedicaFirebaseAuthException(e.code).message;
+    } on FirebaseException catch (e) {
+      throw MedicaFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw const MedicaFormatException();
+    } on PlatformException catch (e) {
+      throw MedicaPlatformException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again!';
+    }
+  }
+
   /// [EmailAuthentication] - ReAuthenticate User
   /// [EmailAuthentication] - Forget Password
 /* --------- Federated identity & social sign-in --------- */
