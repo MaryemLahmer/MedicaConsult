78ed2e6c0b392d6d1a37caf08f8e64f96c12dbbc
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-04-25 01:16:04 +0100

    create login controller

---
+++ lib/features/authentication/controllers/login/login_controller.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:medica_consult/data/repositories/authentication/authentication_repo.dart';
+import 'package:medica_consult/utils/helpers/loaders.dart';
+import 'package:medica_consult/utils/helpers/network_manager.dart';
+import 'package:medica_consult/utils/popups/full_screen_loader.dart';
+
+import '../../../../utils/constants/image_strings.dart';
+
+class LoginController extends GetxController {
+
+  /// Variables
+  final rememberMe = false.obs;
+  final hidePassword = true.obs;
+  final localStorage = GetStorage();
+  final email = TextEditingController();
+  final password = TextEditingController();
+  GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
+
+  @override
+  void onInit() {
+    email.text = localStorage.read('REMEMBER_ME_EMAIL') ?? '';
+    password.text = localStorage.read('REMEMBER_ME_PASSWORD') ?? '';
+    super.onInit();
+  }
+
+  /// --- Email and Password Sign In
+  Future <void> emailAndPasswordSignIn() async {
+    try {
+      // Start Loading
+      FullScreenLoader.openLoadingDialog(
+          'Logging In', MedicaImages.loadingScreen);
+
+      // Check Internet Connectivity
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) {
+        FullScreenLoader.stopLoading();
+        return;
+      }
+
+      // Form Validation
+      if (loginFormKey.currentState!.validate()) {
+        FullScreenLoader.stopLoading();
+        return;
+      }
+
+      // Save data if Remember Me is selected
+      if (rememberMe.value) {
+        localStorage.write('REMEMBER_ME_EMAIL', email.text.trim());
+        localStorage.write('REMEMBER_ME_PASSWORD', password.text.trim());
+      }
+
+      // Login user using Email and Password authentication
+      final userCredentials = await AuthenticationRepository.instance
+          .loginWithEmailAndPassword(email.text.trim(), password.text.trim());
+
+      // Remove Loader
+      FullScreenLoader.stopLoading();
+
+      // Redirect
+      AuthenticationRepository.instance.screenRedirect();
+    } catch (e) {
+      FullScreenLoader.stopLoading();
+      Loaders.errorSnackBar(title: 'Oh Snap', message: e.toString());
+    }
+  }
+
+
+}
