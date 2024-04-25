e97e5856c74189c6cae3d02e00a764b86becd4e9
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-04-25 01:15:06 +0100

    update user repo

--- lib/data/repositories/user/user_repository.dart
+++ lib/data/repositories/user/user_repository.dart
@@ -1,7 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import '../../../features/personalization/models/user_model.dart';
+import '../../../utils/exceptions/firebase_auth_exceptions.dart';
+import '../../../utils/exceptions/firebase_exceptions.dart';
+import '../../../utils/exceptions/format_exceptions.dart';
+import '../../../utils/exceptions/platform_exceptions.dart';
 
 /// repository class for user-related operations
 class UserRepository extends GetxController {
@@ -13,18 +18,20 @@ class UserRepository extends GetxController {
   Future<void> saveUserRecord(UserModel user) async {
     try {
       await _db.collection("Users").doc(user.id).set(user.toJson());
+    } on FirebaseAuthException catch (e) {
+      throw MedicaFirebaseAuthException(e.code).message;
     } on FirebaseException catch (e) {
-      throw 'error message';
+      throw MedicaFirebaseException(e.code).message;
     } on FormatException catch (_) {
-      throw 'error message';
+      throw const MedicaFormatException();
     } on PlatformException catch (e) {
-      throw 'error message';
+      throw MedicaPlatformException(e.code).message;
     } catch (e) {
-      throw 'Something went wrong, please try again!';
+      throw 'Something went wrong. Please try again!';
     }
   }
 
   /// Function to fetch user details based on user ID
   /// Function to update User data in Firestore
-  ///
+  /// Update any field in specific users Collections
 }
