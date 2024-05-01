3c267109a227472322d89240ffd95beb0d384d82
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-05-01 21:18:14 +0100

    adding google sign in button

--- lib/common/login_signup/social_buttons.dart
+++ lib/common/login_signup/social_buttons.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:medica_consult/features/authentication/controllers/login/login_controller.dart';
 import '../../utils/constants/colors.dart';
 import '../../utils/constants/image_strings.dart';
 import '../../utils/constants/sizes.dart';
+import 'package:get/get.dart';
 
 class SocialButtons extends StatelessWidget {
   const SocialButtons({
@@ -10,6 +12,7 @@ class SocialButtons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(LoginController());
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -18,7 +21,7 @@ class SocialButtons extends StatelessWidget {
               border: Border.all(color: MedicaColors.grey),
               borderRadius: BorderRadius.circular(100)),
           child: IconButton(
-              onPressed: () {},
+              onPressed: () => controller.googleSignIn(),
               icon: const Image(
                   width: MedicaSizes.iconMd,
                   height: MedicaSizes.iconMd,
