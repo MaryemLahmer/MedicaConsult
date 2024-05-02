e114e52b96c722718f44c0ddb453b12ca4e1df64
MaryemLahmer <121202537+MaryemLahmer@users.noreply.github.com>
2024-05-03 00:16:12 +0100

    fetch data from firestore to display in appbar

--- lib/common/widgets/appbar/App_Bar_Profile_Card.dart
+++ lib/common/widgets/appbar/App_Bar_Profile_Card.dart
@@ -1,32 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
+import 'package:medica_consult/features/personalization/controllers/user_controller.dart';
 import 'package:medica_consult/utils/helpers/helper_function.dart';
-
 import '../../../utils/constants/colors.dart';
-import '../../../utils/constants/image_strings.dart';
 import '../../../utils/constants/sizes.dart';
+import 'package:get/get.dart';
+
+import '../loaders/shimmer_effect.dart';
 
 class UserImageAndNameWIthIcon extends StatelessWidget {
-  const UserImageAndNameWIthIcon({
-    super.key,
-    required this.height,
-    required this.width,
-    required this.radius,
-    required this.imageName,
-    required this.title,
-    required this.subtitle,
-    this.icon,
-    this.iconSize,
-    this.onPressed,
-    this.rating
-  });
+  const UserImageAndNameWIthIcon(
+      {super.key,
+      required this.height,
+      required this.width,
+      required this.radius,
+      required this.imageName,
+      required this.title,
+      this.icon,
+      this.iconSize,
+      this.onPressed,
+      this.rating});
 
   final double height;
   final double width;
   final double radius;
   final String imageName;
   final String title;
-  final String subtitle;
   final IconData? icon;
   final double? iconSize;
   final VoidCallback? onPressed;
@@ -34,6 +32,7 @@ class UserImageAndNameWIthIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(UserController());
     final dark = MedicaHelperFunctions.isDarkMode(context);
     return SafeArea(
       child: Row(
@@ -55,17 +54,33 @@ class UserImageAndNameWIthIcon extends StatelessWidget {
                 children: [
                   Text(
                     title,
-                    style: Theme.of(context).textTheme.titleLarge,
+                    style: Theme.of(context)
+                        .textTheme
+                        .labelMedium!
+                        .apply(color: MedicaColors.white),
                   ),
                   const SizedBox(
                     height: MedicaSizes.xs,
                   ),
-                  Text(subtitle,
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyMedium!
-                          .apply(color: dark? MedicaColors.white:MedicaColors.darkerGrey)),
-                  Padding(padding: const EdgeInsets.all(MedicaSizes.xs/3),child: rating,)
+                  Obx(() {
+                    if (controller.profileLoading.value) {
+                      // Display a shimmer loader while user profile is being loaded
+                      return const ShimmerEffect(width: 80, height: 15);
+                    } else {
+                      return Text(controller.user.value.fullName,
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineSmall!
+                              .apply(
+                                  color: dark
+                                      ? MedicaColors.white
+                                      : MedicaColors.black));
+                    }
+                  }),
+                  Padding(
+                    padding: const EdgeInsets.all(MedicaSizes.xs / 3),
+                    child: rating,
+                  )
                 ],
               ),
             ),
